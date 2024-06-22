473db9f33286403437f099ab3ddc04e62754439a
v-nils <mail@nils-voss.com>
2024-06-22 15:47:07 +0200

    Changed data paths

--- src/interpol.py
+++ src/interpol.py
@@ -8,18 +8,20 @@ import scienceplots
 plt.style.use(['science', 'ieee'])
 
 # Load the dataset
-file_path = '../data/pre_processed/in_and_out_filtered.csv'
-out_path = '../data/pre_processed/in_and_out_filled_vol.csv'
+file_path = '../data/pre_processed/processing_dataset.csv'
+out_path = '../data/pre_processed/interpolated_ds.csv'
 plot_path = '../data/results/interpolation/interpolation.png'
 data = pd.read_csv(file_path)
 
+print(data.columns)
+
 # Separate the data with and without volume values
-data_with_volume = data[data['Volume'] > 0]
-data_without_volume = data[data['Volume'] == 0]
+data_with_volume = data[data['volume'] > 0]
+data_without_volume = data[data['volume'] == 0]
 
 # Extract the relevant columns
-X = data_with_volume[['SArea']].values.reshape(len(data_with_volume[['SArea']].values), )
-y = data_with_volume['Volume'].values
+X = data_with_volume[['sarea']].values.reshape(len(data_with_volume[['sarea']].values), )
+y = data_with_volume['volume'].values
 
 # Scale data
 scaler = StandardScaler()
@@ -46,18 +48,18 @@ X_new = np.linspace(np.min(X), np.max(X), 1000)
 y_fit = np.polyval(p, X_new)
 
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 9))
-ax.tick_params(axis='both', colors='black', labelsize=12)
-ax.scatter(X, y, marker='x', s=15, alpha=0.5, label='Data Points')
+fig, ax = plt.subplots(1, 1)
+ax.tick_params(axis='both', colors='black', labelsize=9)
+ax.scatter(X, y, marker='x', s=5, alpha=0.5, label='Data Points')
 ax.plot(X_new, y_fit, color='red', label='Polynomial Fit', linestyle='--')
-ax.set_xlabel('Surface Area', fontsize=16)
-ax.set_ylabel('Volume', fontsize=16)
-ax.set_title(r'${}^{}={}$'.format('R', 2, np.round(r2, 2)), fontsize=24)
-plt.legend(fontsize=16)
+ax.set_xlabel('Surface Area', fontsize=9)
+ax.set_ylabel('Volume', fontsize=9)
+ax.set_title(r'${}^{}={}$'.format('R', 2, np.round(r2, 2)), fontsize=14)
+plt.legend(fontsize=9)
 plt.savefig(plot_path)
 
 
-data[data['Volume'] == 0] = data[data['Volume'] == 0].assign(Volume=np.polyval(p, data[data['Volume'] == 0]['SArea']))
+data[data['volume'] == 0] = data[data['volume'] == 0].assign(Volume=np.polyval(p, data[data['volume'] == 0]['sarea']))
 
 # Save the data
 data.to_csv(out_path, index=False)
